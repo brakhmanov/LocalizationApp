52e6aa779ad858bb87175f77a05fcdcff4c736cd
brakhmanov <dumorph@gmail.com>
2023-12-06 23:15:37 +0100

    adding localization for text

--- app/src/main/java/com/example/localizationapp/MainActivity.kt
+++ app/src/main/java/com/example/localizationapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.localizationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var greetingView : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        greetingView = findViewById(R.id.greetingView)
+
+        val button : Button = findViewById(R.id.button)
+
+        button.setOnClickListener{
+            changeGreeting()
+        }
+
+    }
+
+    fun changeGreeting() {
+        greetingView.text = getString(R.string.new_greeting)
     }
 }
